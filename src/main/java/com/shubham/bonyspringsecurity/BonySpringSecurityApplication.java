69e46b84d2df27c72fcf4f33cf839f23763c410e
ShubhamTripathi1991 <141986272+ShubhamTripathi1991@users.noreply.github.com>
2024-01-23 09:44:41 +0530

    Initial Commit

---
+++ src/main/java/com/shubham/bonyspringsecurity/BonySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.bonyspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonySpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonySpringSecurityApplication.class, args);
+	}
+
+}
