03f917c60098960ebe218baabb0719da00f73278
ShubhamTripathi1991 <141986272+ShubhamTripathi1991@users.noreply.github.com>
2024-03-13 14:30:35 +0530

    test

---
+++ bony-spring-security/src/main/java/com/shubham/bonyspringsecurity/controller/SpringSecurityTestController.java
@@ -0,0 +1,16 @@
+package com.shubham.bonyspringsecurity.controller;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class SpringSecurityTestController {
+
+	@GetMapping("/csrf-token")
+	public CsrfToken getCsrfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
+	}
+}
--- bony-spring-security/src/main/java/com/shubham/bonyspringsecurity/controller/TodoController.java
+++ bony-spring-security/src/main/java/com/shubham/bonyspringsecurity/controller/TodoController.java
@@ -2,25 +2,35 @@ package com.shubham.bonyspringsecurity.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 
-	private static final List<Todo> TODOS_LIST = List.of(new Todo("shubham", "Hello"),
-			new Todo("shubham1", "Hello1"));
+	private static final List<Todo> TODOS_LIST = List.of(new Todo("shubham", "Hello"), new Todo("shubham1", "Hello1"));
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@GetMapping("/todos")
 	public List<Todo> getAllTodos() {
 		return TODOS_LIST;
 	}
-	
+
 	@GetMapping("users/{username}/todos")
 	public Todo getTodoByUsername(@PathVariable("username") String username) {
 		return TODOS_LIST.get(0);
 	}
+
+	@PostMapping("users/{username}/todos")
+	public void createTodoForSpecificUSer(@PathVariable("username") String username, @RequestBody Todo todo) {
+		logger.info("Create {} for {}", todo, username);
+	}
 }
 
-record Todo(String username, String description) {}
+record Todo(String username, String description) {
+}
